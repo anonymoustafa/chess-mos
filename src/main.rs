ab8adff05379433d8675ebdfc8dcde0f76fb58c3
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-01-16 11:24:04 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+fn main() {
+
+    let choices = vec!["Option 1", "Option 2", "Option 3"];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Choose an option:")
+        .items(&choices)
+        .default(0) // Default selection (optional)
+        .interact()
+        .unwrap();
+
+    println!("You chose: {}", choices[selection]);
+
+
+        while true {
+    println!("Welcome to the chess-mos! ");
+    println!("hello");
+}
+}
